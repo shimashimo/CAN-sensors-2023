a2f0858179911e8f856791636110f630fe338129
Ewan Chatham <echatham@outlook.com>
2025-04-20 16:53:31 -0700

    Cleaning up and adding some notes

--- engine_shifter/engine_shifter.ino
+++ engine_shifter/engine_shifter.ino
@@ -1,3 +1,10 @@
+/* Notes:
+  - Reverse Mode increases encoder value
+  - Forward Mode decreases encoder value
+  TODO: Change UP_SHIFT and DOWN_SHIFT setpoints to the engine's liking
+  If up shifting and down shifting is reversed, change the sign of the setpoints... Should work?
+*/
+
 #define ENCODER_OPTIMIZE_INTERRUPTS // For optimized interrupts in encoder
 #include <Encoder.h>
 #include <Servo.h>
@@ -36,7 +43,6 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 double oldPosition = -99;
 
 unsigned long neutralStartTime;
-bool inNeutralState;
 const unsigned long NEUTRAL_TIMEOUT = 200;  // Time in milliseconds to wait before returning
 
 void setup() {
@@ -57,57 +63,35 @@ void setup() {
   pinMode(MOTOR_ON_PIN, INPUT_PULLUP);
 
   neutralStartTime = 0;  // Time when we first entered neutral state
-  inNeutralState = false;  // Track if we're in neutral state
 }
 
-// Reverse Mode increases encoder value
-//  
+
 
 void loop() {
-  // Update PID input with the current encoder position
-  Input = myEnc.read();
-  
   bool shiftUpState = digitalRead(SHIFT_UP_PIN);
   bool shiftDownState = digitalRead(SHIFT_DOWN_PIN);
   bool neutralState = digitalRead(SHIFT_NEUTRAL_PIN);
   bool motorOnState = digitalRead(MOTOR_ON_PIN);
-
-  // if (Input != oldPosition) {
-  //   oldPosition = Input;
-  //   Serial.print("Encoder Position: ");
-  //   Serial.println(Input);
-  // }
-
-  // Compute PID output
-  myPID.Compute();
+  
   // Serial.print("Output Value: ");
   // Serial.println(Output);
-  // Output is distance from setpoint - i.e error
-
+  
 
   if (shiftUpState == LOW && prevShiftUpState == HIGH) {
     Serial.println("Shift Up Pressed");
-    // Setpoint = UP_SHIFT_SETPOINT;
     sparkmax.writeMicroseconds(2000);
     while(Input > UP_SHIFT_SETPOINT) {Input = myEnc.read();}
-    Prev_setpoint = UP_SHIFT_SETPOINT;
-    inNeutralState = false;
   }
  
   if (shiftDownState == LOW && prevShiftDownState == HIGH) {
     Serial.println("Shift Down Pressed");
-    // Setpoint = DOWN_SHIFT_SETPOINT;
     sparkmax.writeMicroseconds(1000);
     while(Input < DOWN_SHIFT_SETPOINT) {Input = myEnc.read();}
-    Prev_setpoint = DOWN_SHIFT_SETPOINT;
-    inNeutralState = false;
   }
 
   if (neutralState == LOW && prevNeutralState == HIGH) {
-    // Serial.println("Neutral Pressed");
+    Serial.println("Neutral Pressed");
     Setpoint = NEUTRAL_SETPOINT;
-    Prev_setpoint = NEUTRAL_SETPOINT;
-    inNeutralState = false;
   }
 
   // Update previous button states
@@ -118,52 +102,37 @@ void loop() {
 
   delay(20);  // Button debounce
 
+
+  // Update PID input with the current encoder position
+  Input = myEnc.read();
+  // Compute PID output
+  myPID.Compute();
+
   if(Output < -700) { // Error is large = position - setpoint. Need to increase encoder value, which is reversing motor
-    // Full Reverse: p <= 1000
+    /*  Full Reverse: p <= 1000 */
     // Serial.println("FULL REVERSE");
     sparkmax.writeMicroseconds(1000);
-    inNeutralState = false;
   } 
   else if(Output >= -700 && Output <= -10) {
-    // Proportional Reverse: 1000 < p < 1475
+    /* Proportional Reverse: 1000 < p < 1475 */
     // Serial.println("PROP REVERSE");
     int speed = map(Output, -10, -700, 1450, 1100);
     sparkmax.writeMicroseconds(speed);
-    inNeutralState = false;
   } 
   else if(Output > 700) { 
-    // Full Forward: p >= 2000
+    /* Full Forward: p >= 2000 */
     // Serial.println("FULL FORWARD");
     sparkmax.writeMicroseconds(2000);
-    inNeutralState = false;
   }
   else if(Output >= 10 && Output <= 700) {
-    // Proportional Forward: 1525 < p < 2000
+    /* Proportional Forward: 1525 < p < 2000 */
     // Serial.println("PROP FORWARD");
     int speed = map(Output, 700, 10, 1900, 1550);
     sparkmax.writeMicroseconds(speed);
-    inNeutralState = false;
   }
-  // else if (Output == 0) {
   else if(Output > -10 && Output < 10) { // Set Neutral
-  // else {
-    // Neutral: 1475 <= p <= 1525
+    /* Neutral: 1475 <= p <= 1525 */
     // Serial.println("Neutral");
     sparkmax.writeMicroseconds(1500);
-    if (!inNeutralState) {
-      // First time entering neutral state, record the time
-      neutralStartTime = millis();
-      inNeutralState = true;
-    }
-    else if ((millis() - neutralStartTime) >= NEUTRAL_TIMEOUT) {
-      // We've been in neutral state for the required time
-      // Serial.println("Neutral timeout reached, returning");
-      delay(20);
-      if(Setpoint != NEUTRAL_SETPOINT && Prev_setpoint != NEUTRAL_SETPOINT) {
-        // Serial.println("Setting NEUTRAL");
-        Setpoint = NEUTRAL_SETPOINT;
-        inNeutralState = false;
-      }
-    }
   }
 }
